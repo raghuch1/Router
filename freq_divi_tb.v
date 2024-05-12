1711b64f023c60fc90bf62b5042d2871fb165c85
raghuch1 <raghucheepurupalli1234@gmail.com>
2024-05-13 00:00:10 +0530

    Create freq_divi_tb.v

---
+++ freq_divi_tb.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.05.2024 21:51:26
+// Design Name: 
+// Module Name: freq100
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module freq100_tb();
+reg clock,rst;
+reg [1:0]sel;
+wire out;
+
+freq_divi dut(clock,rst,sel,out);
+//clock for 100 MHZ frequency....
+initial
+      begin 
+          clock=1'b0;
+       forever
+           #5 clock=~clock;
+      end
+initial 
+begin
+sel=3;
+rst=1;
+#10;
+rst=0;
+
+end
+endmodule
